c63878ded9998db590369c90272a969754c3f092
walnuts1018 <r.juglans.1018@gmail.com>
2023-05-10 14:22:41 +0900

    fix

--- init.zsh
+++ init.zsh
@@ -5,10 +5,11 @@ fi
 
 export EXA_COLORS='da=1;34:gm=1;34'
 
-alias ls='exa --group-directories-first'
-alias ll='ls -l --git'        # Long format, git status
+alias ls='exa --git -@ -mU --icons --time-style=long-iso --color=automatic --group-directories-first'
+alias ll='ls -lh'        # Long format, git status
 alias l='ll -a'               # Long format, all files
-alias lr='ll -T'              # Long format, recursive as a tree
+alias lr='ls -Tlh'              # Long format, recursive as a tree
+alias tree='ls -Tl --no-permissions --no-filesize --no-user --no-time'
 alias lx='ll -sextension'     # Long format, sort by extension
 alias lk='ll -ssize'          # Long format, largest file size last
 alias lt='ll -smodified'      # Long format, newest modification time last
