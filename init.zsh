88091d43728eeb041086443be5be70888518e935
walnuts1018 <r.juglans.1018@gmail.com>
2023-05-10 20:31:34 +0900

    add group

--- init.zsh
+++ init.zsh
@@ -5,7 +5,7 @@ fi
 
 export EXA_COLORS='da=1;34:gm=1;34'
 
-alias ls='exa --git -@ -mU --icons --time-style=long-iso --color=automatic --group-directories-first'
+alias ls='exa --git -@ -g -mU --icons --time-style=long-iso --color=automatic --group-directories-first'
 alias ll='ls -lh'        # Long format, git status
 alias l='ll -a'               # Long format, all files
 alias lr='ls -Tlh'              # Long format, recursive as a tree
