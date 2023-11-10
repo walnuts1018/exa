f735e2ca88741f167c7067ba183dd7380a1b029e
walnuts1018 <r.juglans.1018@gmail.com>
2023-11-10 23:34:18 +0900

    test

--- init.zsh
+++ init.zsh
@@ -1,17 +1,18 @@
 # Ensure exa is available
 if (( ${+commands[eza]} )); then
-  alias ls='eza --group-directories-first'
+  alias ls='eza --git -@ -g -mU --icons --time-style=long-iso --color=automatic --group-directories-first'
 elif (( ${+commands[exa]} )); then
-  alias ls='exa --group-directories-first'
+  alias ls='exa -@ -g -mU --icons --time-style=long-iso --color=automatic --group-directories-first'
 else
   return 1
 fi
 
 export EXA_COLORS='da=1;34:gm=1;34'
 
-alias ll='ls -l --git'        # Long format, git status
+alias ll='ls -lh'        # Long format, git status
 alias l='ll -a'               # Long format, all files
-alias lr='ll -T'              # Long format, recursive as a tree
+alias lr='ls -Tlh'              # Long format, recursive as a tree
+alias tree='ls -Tl --no-permissions --no-filesize --no-user --no-time'
 alias lx='ll -sextension'     # Long format, sort by extension
 alias lk='ll -ssize'          # Long format, largest file size last
 alias lt='ll -smodified'      # Long format, newest modification time last
